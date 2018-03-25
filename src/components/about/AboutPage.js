39bd07ee392bd83eb2826eea59ee0b3e3be6f7ec
José De Ita <josdeita@gmail.com>
2018-03-25 08:39:52 -0600

    add initial app structure

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class AboutPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum eos est, veritatis fuga repudiandae animi labore. Tempore, praesentium? Tenetur, laudantium illum dolor eaque voluptatum error soluta quas. Velit, fugiat vitae.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
---
+++ src/components/home/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <div className="container">
+          <h1 className="display-3">Hello, world!</h1>
+          <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+          <Link to="about" className="btn btn-primary btn-lg">About</Link>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
