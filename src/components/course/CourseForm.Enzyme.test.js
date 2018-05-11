6888c34378e239156fbf55e47526dfb36650135a
José De Ita <josdeita@gmail.com>
2018-05-11 13:21:10 -0500

    Testing with Enzyme

---
+++ src/components/course/CourseForm.Enzyme.test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import expect from 'expect';
+import { mount, shallow } from 'enzyme';
+import CourseForm from './CourseForm';
+
+function setup(saving = true) {
+  let props = {
+    course: {},
+    saving: saving,
+    errors: {},
+    onSave: () => {},
+    onchange: () => {}
+  };
+
+  return shallow(<CourseForm {...props} />);
+}
+
+describe('CourseForm via enzyme', () => {
+  it('render form', () => {
+    const wrapper = setup();
+
+    expect(
+      wrapper.find('form').length
+    ).toBe(1);
+  });
+
+  it('save button is labeled "Save" when not saving', () => {
+    const wrapper = setup(false);
+
+    expect(wrapper.find('input').props().value).toBe('Save');
+  });
+
+  it('save button is labeled "Saving..." when saving', () => {
+    const wrapper = setup();
+
+    expect(wrapper.find('input').props().value);
+  });
+});
